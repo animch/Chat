2b3862459e12d2c4f37acc4c304529eef934e379
animch <nimchuk996@gmail.com>
2023-07-16 23:17:18 +1000

    add NotFoundPage

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import NotFound from './NotFoundPage.jsx';
+import LoginPage from './LoginPage.jsx';
+
+const App = () => (
+  <div className="h-100">
+    <div className="d-flex flex-column h-100">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,7 @@
+export default {
+    translation: {
+        notFound: 'Страница не найдена',
+        youCanGo: 'Но вы можете перейти',
+        toHomePage: 'на главную страницу',
+    },
+};

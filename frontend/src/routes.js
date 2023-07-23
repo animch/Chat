16ee914446899a44bfcaf6d6e22efed66fa82537
animch <nimchuk996@gmail.com>
2023-07-23 23:26:44 +1000

    add route

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,9 @@
+const apiPath = '/api/v1';
+
 export default {
   chatPagePath: () => '/',
   loginPagePath: () => '/login',
   signUpPagePath: () => '/signup',
+
+  loginPath: () => [apiPath, 'login'].join('/'),
 };

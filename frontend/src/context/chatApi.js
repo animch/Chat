48116ee38160c4b4b60923eff4fe07f576d63501
animch <nimchuk996@gmail.com>
2023-08-16 21:39:22 +1000

    refactoring

--- frontend/src/context/chatApi.js
+++ frontend/src/context/chatApi.js
@@ -1,14 +1,4 @@
 import { io } from 'socket.io-client';
-import store, { actions } from '../slices/index.js';
-
-const {
-  addMessage,
-  addChannel,
-  deleteChannel,
-  channelRename,
-} = actions;
-
-const { dispatch } = store;
 
 const socket = io();
 
@@ -29,18 +19,5 @@ const chatApi = {
   removeChannel: (id) => emitPromise('removeChannel', { id }),
   renameChannel: ({ name, id }) => emitPromise('renameChannel', { name, id }),
 };
-
-socket.on('newMessage', (payload) => {
-  dispatch(addMessage(payload));
-});
-socket.on('newChannel', (payload) => {
-  dispatch(addChannel(payload));
-});
-socket.on('removeChannel', (payload) => {
-  dispatch(deleteChannel(payload));
-});
-socket.on('renameChannel', (payload) => {
-  dispatch(channelRename(payload));
-});
 
 export default chatApi;
--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -3,12 +3,12 @@ import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import leoProfanity from 'leo-profanity';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
-
+import { io } from 'socket.io-client';
 import AuthProvider from './context/AuthProvider.jsx';
 import { chatApiContext } from './context/context.js';
 import App from './components/App.jsx';
 import resources from './locales/index.js';
-import store from './slices/index.js';
+import store, { actions } from './slices/index.js';
 import chatApi from './context/chatApi.js';
 
 const Init = async () => {
@@ -19,6 +19,30 @@ const Init = async () => {
       resources,
       fallbackLng: 'ru',
     });
+
+  const { dispatch } = store;
+
+  const {
+    addMessage,
+    addChannel,
+    deleteChannel,
+    channelRename,
+  } = actions;
+
+  const socket = io();
+
+  socket.on('newMessage', (payload) => {
+    dispatch(addMessage(payload));
+  });
+  socket.on('newChannel', (payload) => {
+    dispatch(addChannel(payload));
+  });
+  socket.on('removeChannel', (payload) => {
+    dispatch(deleteChannel(payload));
+  });
+  socket.on('renameChannel', (payload) => {
+    dispatch(channelRename(payload));
+  });
 
   const ruDict = leoProfanity.getDictionary('ru');
   leoProfanity.add(ruDict);
